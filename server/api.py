e4249ae583fdcf60f30effc8be285a717d9e4eb7
alisoltanirad <soltaniradali@gmail.com>
2021-04-01 13:35:44 -0400

    Add and register blueprints

--- server/api.py
+++ server/api.py
@@ -1,4 +1,22 @@
-from data import Dataset
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from server.data import Dataset
+
+bp = Blueprint('api', __name__, url_prefix='/')
+
+
+@bp.route('/', methods=['GET'])
+def characters():
+    return 'Characters Placeholder!'
+
+
+@bp.route('/character/<int:id>', methods=['GET'])
+def character_by_id(id: int):
+    return 'Character {id} Placeholder!'.format(id=id)
 
 
 def get_characters():
