c45382dbfea6b0d695d0379b1519681e0b5dc9de
alisoltanirad <soltaniradali@gmail.com>
2021-04-02 14:57:04 -0400

    Changed file name

--- static/src/index.js
+++ static/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Characters from './js/character.js';
+import Characters from './js/characters.js';
 
 ReactDom.render(<Characters />, document.getElementById('character-cards'))
 ReactDom.render(<p>Placeholder</p>, document.getElementById('filters'))
